cc91716d2213ef48e1d62b6b313f0d628a2b0710
Ulyses Rico <ulyses@quart.com.mx>
2020-08-31 13:51:10 -0500

    ...

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ from cassandra.query import SimpleStatement
 
 pathtohere=os.getcwd()
 #nltk.download('stopwords')
+"""
+   Start ML with 10th period
+"""
 def main():
     
     cloud_config= {
@@ -22,20 +25,26 @@ def main():
     auth_provider = PlainTextAuthProvider(objCC.cc_user,objCC.cc_pwd)
     
     cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
-    #session = cluster.connect()
+    session = cluster.connect()
     
-    sw=stopwords.words('spanish')
-    print(sw)
-    print('ML with python')
+    #sw=stopwords.words('spanish')
+    #print(sw)
+    #print('ML with python')
 
-    querySt="select * from test.tbthesis where period_number>4 ALLOW FILTERING "   
+    querySt="select * from thesis.tbthesis where period_number=10 ALLOW FILTERING"   
         
     count=0
     row=''
     statement = SimpleStatement(querySt, fetch_size=1000)
-    
+    ltDocuments=[]
+
     for row in session.execute(statement):
-        count=count+1
+        thesis=[]
+        for col in row:
+            thesis.append(col)
+        ltDocuments.append(thesis)
+
+     
         
     
 class CassandraConnection():
