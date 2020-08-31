495e6a568e22f3394ece2d9190f14333b2572828
Ulyses Rico <ulyses@quart.com.mx>
2020-08-31 09:59:52 -0500

    ...

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
+import os
+from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
 
-
-
-
-
+pathtohere=os.getcwd()
 
 def main():
-    print('ML with python')
+    
+    cloud_config= {
 
+        'secure_connect_bundle': pathtohere+'//secure-connect-dbquart.zip'
+         
+    }
+    
+    objCC=CassandraConnection()
+   
+    auth_provider = PlainTextAuthProvider(objCC.cc_user,objCC.cc_pwd)
+    
+    cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
+    session = cluster.connect()
+    print('ML with python')
+    
+
+
+
+
+
+class CassandraConnection():
+    cc_user='quartadmin'
+    cc_pwd='P@ssw0rd33'
 
 
 if __name__=='__main__':
